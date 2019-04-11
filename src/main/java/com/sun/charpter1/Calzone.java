5fddc1f5d9420d1e398b5b942a2e1376ad3c5835
ValoraSun <Valora.Sun@gmail.com>
2019-04-11 23:46:25 +0800

    build模式

---
+++ src/main/java/com/sun/charpter1/Calzone.java
@@ -0,0 +1,34 @@
+package com.sun.charpter1;
+
+import static com.sun.charpter1.Pizza.Topping.HAM;
+
+public class Calzone extends Pizza {
+
+    private final boolean sauceInside;
+
+    public static class Builder extends Pizza.Builder<Builder> {
+        private boolean sauceInside = false;
+
+        public Builder sauceInside() {
+            sauceInside = true;
+            return this;
+        }
+
+        @Override
+        Calzone build() {
+            return new Calzone(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+    private Calzone(Builder builder) {
+        super(builder);
+        sauceInside = builder.sauceInside;
+    }
+
+    Calzone calzone = new Builder().addTopping(HAM).sauceInside().build();
+}
--- src/main/java/com/sun/charpter1/NyPizza.java
+++ src/main/java/com/sun/charpter1/NyPizza.java
@@ -2,6 +2,10 @@ package com.sun.charpter1;
 
 import java.util.Objects;
 
+import static com.sun.charpter1.NyPizza.Size.SMALL;
+import static com.sun.charpter1.Pizza.Topping.ONION;
+import static com.sun.charpter1.Pizza.Topping.SAUSAGE;
+
 public class NyPizza extends Pizza {
 
     public enum Size {SMALL, MEDIUM, LARGE}
@@ -31,5 +35,7 @@ public class NyPizza extends Pizza {
         super(builder);
         size = builder.size;
     }
+
+    NyPizza pizza = new NyPizza.Builder(SMALL).addTopping(SAUSAGE).addTopping(ONION).build();
 
 }
