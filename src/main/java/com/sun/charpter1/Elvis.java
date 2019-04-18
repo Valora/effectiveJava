c3132e9470ab0f1c6fd78e87fe404d931dda573c
ValoraSun <Valora.Sun@gmail.com>
2019-04-18 22:44:39 +0800

    instance

---
+++ src/main/java/com/sun/charpter1/Elvis.java
@@ -0,0 +1,12 @@
+package com.sun.charpter1;
+
+public class Elvis {
+    public static final Elvis INSTANCE = new Elvis();
+    private  Elvis() {
+
+    }
+
+    public void leaveTheBuilding() {
+
+    }
+}
