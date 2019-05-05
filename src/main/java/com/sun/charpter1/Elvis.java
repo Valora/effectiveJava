89da56f4b075ac1f134e7b50a48da149314163f0
ValoraSun <Valora.Sun@gmail.com>
2019-05-06 01:24:00 +0800

    singlton

--- src/main/java/com/sun/charpter1/Elvis.java
+++ src/main/java/com/sun/charpter1/Elvis.java
@@ -1,9 +1,20 @@
 package com.sun.charpter1;
 
-public class Elvis {
-    public static final Elvis INSTANCE = new Elvis();
-    private  Elvis() {
+import java.io.Serializable;
 
+public class Elvis implements Serializable {
+    private static final Elvis INSTANCE = new Elvis();
+    private  Elvis() {
+    }
+
+    public static Elvis getInstance() {
+        return INSTANCE;
+    }
+
+
+    //防止每次反序列化一个序列化的实例是，都会创建一个新的实例
+    private Object readResolve() {
+        return INSTANCE;
     }
 
     public void leaveTheBuilding() {
